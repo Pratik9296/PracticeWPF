fcd5a1c48223859ccd2f0f22fc352f5b9dd10cd6
Pratik9296 <pratiksolanki787@gmail.com>
2019-12-04 22:55:18 +0530

    Add File selection dialog box

--- PracticeWPF/PracticeWPF/Form1.cs
+++ PracticeWPF/PracticeWPF/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace PracticeWPF
 {
@@ -15,6 +16,20 @@ namespace PracticeWPF
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "XMl Document (.xml) |*.xml";
+            file.InitialDirectory = @"C:\Users\S\Desktop\documents";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                var selectedFilePath = file.FileName;
+                textBox1.Text = selectedFilePath;
+                XDocument xDoc = XDocument.Load(selectedFilePath);
+                var nodeNames = xDoc.Root.Descendants().Where(x => (!x.HasElements)).Select(x => x.Name).Distinct();
+            }
         }
     }
 }
